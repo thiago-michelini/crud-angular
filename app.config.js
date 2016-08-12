b6bf830095e43245b72a74dda4f274eed520b118
thiago.michelini <thiago.michelini@redetendencia.com.br>
2016-08-12 16:40:10 -0400

    #commit máquina do trabalho

--- app.config.js
+++ app.config.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var NOME_APLICACAO = 'CRUD Angular com Microservice';
+
+var loc = window.location;
+
 angular.
   module('crudUsuarioApp').
   config(['$locationProvider', '$routeProvider',
@@ -8,12 +12,24 @@ angular.
 
       $routeProvider.
         when('/usuario', {
+          resolve: {"check":function(){checarSessaoUsuario()}},
           template: '<usuario-lista></usuario-lista>'
         }).
-        //when('/phones/:phoneId', {
         when('/usuario/:usuarioId', {
+          resolve: {"check":function(){checarSessaoUsuario()}},
           template: '<usuario-detalhe></usuario-detalhe>'
         }).
         otherwise('/usuario');
     }
   ]);
+
+function checarSessaoUsuario() {
+  if (!localStorage.getItem('tnd-user-session')) {
+    var urlLogin = loc.protocol + '//' + loc.host + '/portal?redirect=' + loc.protocol + '//' + loc.host + '/crud-angular';
+    window.location = urlLogin;
+  }
+}
+
+$(function(){
+  $('<script src="' + loc.protocol + '//' + loc.host + '/portal/js/header-apps.js"></script>').insertAfter($('#app-config-js'));
+});
